794f6eb6cc8f02277a6409d6afeddc979ad9bdec
Chandraxxx <79895708+Chandraxxx@users.noreply.github.com>
2021-05-12 10:27:53 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import datetime, timedelta
+from flask_mail import Mail, Message
 import os, pdfkit
 
 path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
 config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
 app = Flask(__name__)
+# Manage Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 # Manage Session
 app.secret_key = "1q2w3e4r5t"
 app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=5)
@@ -26,9 +32,15 @@ def index():
 	return render_template('home.html', greet = greet)
 
 # login
-@app.route('/login')
+@app.route('/login', methods = ['GET','POST'])
 def login():
-	return "Kat"
+        if request.method == "POST":
+                username = request.form['uname']
+                password = request.form['pw']
+                session['uname'] = username
+                return redirect(url_for('index'))
+        else:
+                return render_template('login.html')
 
 #downloadlistPage
 @app.route('/list_download')
@@ -41,14 +53,35 @@ def listUser():
 	return render_template('list_user.html')
 
 # helpCentrePage
-@app.route('/help_centre')
+@app.route('/help_centre',methods=['GET','POST'])
 def helpCentre():
-	return render_template('help_centre.html')
+        if request.method == "POST":
+                g_username = request.form['gmail_username']
+                g_password = request.form['gmail_pw']
+                to = request.form['to']
+                subject = request.form['subject']
+                message = request.form['message']
+
+                app.config['MAIL_USERNAME'] = g_username
+                app.config['MAIL_PASSWORD'] = g_password
+
+                msg = Message(subject, sender=g_username, recipients=[to])
+                msg.body = message
+
+                try:
+                        mail = Mail(app)
+                        mail.connect()
+                        mail.send(msg)
+                        return render_template('help_centre.html', success = "True")
+                except:
+                        return render_template('help_centre.html', failed = "True")
+        return render_template('help_centre.html', failed = "False", success = "False")
 
 # logout
 @app.route('/logout')
 def logOut():
-	return "Cyka Blyat"
+        session.pop('uname', None)
+        return redirect(url_for("login"))
 
 if __name__=='__main__':
 	app.run(debug=True)
